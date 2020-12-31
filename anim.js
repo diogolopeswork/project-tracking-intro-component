8ac50821eb4051ca9acab36b0af7a1692c652644
Diogo Lopes <deosl98@gmail.com>
2020-12-31 17:55:55 +0000

    commit

---
+++ anim.js
@@ -0,0 +1,28 @@
+const header = document.querySelector('header');
+const leftPanel = document.querySelector('.left-panel');
+const rightPanel = document.querySelector('.right-panel');
+const illustration = document.getElementById('illustration');
+
+const tl = new TimelineMax();
+
+tl.fromTo(
+    illustration,
+    1,
+    { height: "0%" },
+    { height: "100%", ease: Power2.easeInOut }
+)
+
+    .fromTo(
+        header,
+        1,
+        { x: "-100%" },
+        { x: "0%", ease: Power2.easeInOut }
+    )
+
+    .fromTo(
+        leftPanel,
+        2,
+        { x: "-100%" },
+        { x: "0%", ease: Power2.easeInOut }
+
+    )
--- toggle.js
+++ toggle.js
@@ -12,12 +12,13 @@ $(function() {
         $("#hamburger").hide();
         $("#c-hamburger").show();
         $("#nav").addClass("active");
-        $("#active").show();
+        $("active").show();
     }); 
+
     $(document).on("click", "#c-hamburger", function(e) {
         $("#hamburger").show();
         $("#c-hamburger").hide();
         $("#nav").removeClass("active");
-        $("#active").hide();
+        $("active").hide();
     });
 });
