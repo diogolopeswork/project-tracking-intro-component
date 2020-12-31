f541a2f6ae8c0cc01f8ac022c8d4e576355604e6
Diogo Lopes <deosl98@gmail.com>
2020-12-31 15:06:58 +0000

    commit

--- toggle.js
+++ toggle.js
@@ -1,8 +1,23 @@
-const btn = document.getElementById('hamburger');
-const nav = document.getElementById('nav')
-const close = document.getElementById('close')
+// const btn = document.getElementById('hamburger');
+// const nav = document.getElementById('nav')
+// const close = document.getElementById('close')
 
-btn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    btn.classList.toggle('close');
-})
+// btn.addEventListener('click', () => {
+//     nav.classList.toggle('active');
+//     btn.classList.toggle('close');
+// })
+
+$(function() {
+    $(document).on("click", "#hamburger", function(e) {
+        $("#hamburger").hide();
+        $("#c-hamburger").show();
+        $("#nav").addClass("active");
+        $("#active").show();
+    }); 
+    $(document).on("click", "#c-hamburger", function(e) {
+        $("#hamburger").show();
+        $("#c-hamburger").hide();
+        $("#nav").removeClass("active");
+        $("#active").hide();
+    });
+});
